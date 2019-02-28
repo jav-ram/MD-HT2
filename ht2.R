3040e2cca113c552fde0b6026d86d4c17319b1c0
Unknown <nis16104@uvg.edu.gt>
2019-02-27 22:17:22 -0600

    Demás algoritmos

--- ht2.R
+++ ht2.R
@@ -13,7 +13,8 @@ library(fpc) #para hacer el plotcluster
 library(NbClust) #Para determinar el n?mero de clusters ?ptimo
 library(factoextra) #Para hacer gr?ficos bonitos de clustering
 
-setwd("~/U/2019/1er/MineriaDeDatos")
+#setwd("~/U/2019/1er/MineriaDeDatos")
+setwd("~/Documents/uvg/mineria/MD-HT2")
 movies <- read.csv("tmdb-movies.csv")
 
 movies$id <- NULL
@@ -33,8 +34,7 @@ head(movies)
 
 wss <- (nrow(movies)-1)*sum(apply(movies,2,var))
 
-for (i in 2:10) 
-  wss[i] <- sum(kmeans(movies[,1:4], centers=i)$withinss)
+for (i in 2:10) wss[i] <- sum(kmeans(movies[,1:4], centers=i)$withinss)
 
 plot(1:10, wss, type="b", xlab="Number of Clusters",  ylab="Within groups sum of squares")
 
@@ -46,9 +46,7 @@ datos<-movies
 moviesCompleto<-movies[complete.cases(movies),]
 km<-kmeans(movies, group)
 datos$grupo<-km$cluster
-
 plotcluster(movies,km$cluster) #grafica la ubicación de los clusters
-
 
 #Clustering jerárquico
 hc<-hclust(dist(movies)) #Genera el clustering jerárquico de los datos
@@ -56,6 +54,33 @@ plot(hc) #Genera el dendograma
 rect.hclust(hc,k=group) #Dibuja el corte de los grupos en el gráfico
 groups<-cutree(hc,k=group) #corta el dendograma, determinando el grupo de cada fila
 datos$gruposHC<-groups
-
 g1HC<-datos[datos$gruposHC==1,]
 g2HC<-datos[datos$gruposHC==2,]
+
+#Fuzzy C-Means
+fcm<-cmeans(movies,2)
+datos$FCGrupos<-fcm$cluster
+datos<-cbind(datos,fcm$membership)
+
+#Mixture of gaussians
+mc<-Mclust(movies,2)
+plot(mc, what = "classification", main="MClust Classification")
+datos$mxGau<-mc$classification
+g1MC<-datos[datos$mxGau==1,]
+g2MC<-datos[datos$mxGau==2,]
+
+#Método de la silueta para las k-medias
+silkm<-silhouette(km$cluster,dist(movies))
+mean(silkm[,2]) #0.55, no es la mejor partición pero no está mal
+
+#Método de la silueta para clustering jerárquico
+silch<-silhouette(groups,dist(movies))
+mean(silch[,2]) #0.51, no es la mejor partición pero no está mal
+
+#Método de la silueta para fuzzy cmeans
+silfcm<-silhouette(fcm$cluster,dist(movies))
+mean(silfcm[,2]) #0.54, no es la mejor partición pero no está mal
+
+#Método de la silueta para mixture of gaussians
+silmg<-silhouette(mc$classification,dist(movies))
+mean(silmg[,2]) #0.50, no es la mejor partición pero no está mal
